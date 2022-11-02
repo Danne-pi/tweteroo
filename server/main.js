f29060524cb12e726f79b714841c2395b984ea29
Danne <dannepi.dev@gmail.com>
2022-11-02 19:34:13 -0300

    feat: config between server and front

--- server/main.js
+++ server/main.js
@@ -3,10 +3,16 @@ import cors from "cors";
 
 const app = express()
 app.use(cors())
+app.use(express.json());
+
+app.post("/sign-up", (req, res) => {
+
+    console.log(req.body.username)
+    console.log(req.body.avatar)
+})
 
 
-
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
 
 
 
